d4a215d15e684113b948e16f1fb154642c9d46cc
Miguel Albitres Luna <81734329+Miguel0411@users.noreply.github.com>
2024-07-12 17:14:47 -0500

    Add files via upload

---
+++ 03._OperacionesMatematicas.py
@@ -0,0 +1,18 @@
+#Operaciones Mátematicas
+a = 1;
+b= 2;
+c= 5;
+
+print('Potenciación:',b**c);
+print('Multiplicación:',a*b);
+print('División:',c/b);
+print('parte entera división:',c//b);
+print('Suma:',a+b);
+print('Resta:',b-a);
+print('Módulo o residuo:',c%b);
+
+#Atajos:
+a +=2;
+print(a);
+b-=1;
+print(b)
